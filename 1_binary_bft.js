26eb1486cca32054df03b11660ce04bfba7879ee
Cesar Aguilar <Blueavian9@gmail.com>
2023-06-21 22:27:12 -0700

    Initial commit

--- 1_binary_bft.js
+++ 1_binary_bft.js
@@ -26,7 +26,7 @@ function traverse(root) {
     }
   }
   return values
-}
+
 
 // Test cases: 
 const head = new node(6)
@@ -35,23 +35,124 @@ head.right = new node(9)
 
 console.log(traveres(head)) // 6, 3, 9
 
-
 */
 
 // 1.
 /*
+class node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
 
+function traverse(root) {
+  const value = [];
+  const queue = [];
+  queue.push(root);
+
+while (current.length) {
+  queue.push(current.value);
+}
+if (current.left !== null) {
+  queue.push(current.left);
+}
+if (current.right !== null) {
+  queue.push(current.right);
+  }
+}
+return values; 
+
+
+// Test cases:
+const head = new Node(6);
+  head.left = new Node(3);
+  head.right = new Node(9);
+
+  console.log(traverse(head)); // 6, 3, 9
 */
+
 // 2.
 /* 
+class node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
 
+function traverse(root) {
+  queue.push(root);
+
+while (current.length) {
+  queue.push(current.value);
+}
+if (current.left !== null) {
+  queue.push(current.left);
+}
+if (current.right !== null) {
+  queue.push(current.right);
+ }
+}
+return values;
+
+
+// Test cases: 
+const head = new node(6);
+  head.left = new node(3);
+  head.right = new node(9);
+
+  console.log(traverse(head)); // 6, 3, 9
 */
+
 // 3.
 /*
+class node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
 
+function traverse(root) {
+  const value = [];
+  const queue = [];
+  queue.push(root);
+
+while (current.length) {
+  queue.push(current.value);
+}
+if (current.left !== null) {
+  queue.push(current.left);
+}
+if (current.right !== null) {
+  queue.push(current.right);
+  } 
+}
+return values;
+
+
+// Test cases: 
+const head = new node(6);
+  head.left = new node(3);
+  head.right = new node(9);
+
+console.log(traverse(head)); // 6, 3, 9
 */
+
 // 4.
 /*
+class node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
 function traverse(root) {
   const value = [];
   const queue = [];
@@ -68,7 +169,7 @@ function traverse(root) {
   }
 }
 return values;
-}
+
 
 // Test cases: 
 const head = new node(6);
@@ -77,9 +178,16 @@ const head = new node(6);
 
   console.log(traverse(head)); // 6, 3, 9
 */
-// 5.
 
+// 5.
 /*
+class node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
 function traverse(root) {
   const value = [];
   const queue = [];
@@ -96,20 +204,26 @@ if (current.right !== null) {
  }
 }
 return values; 
-}
+
 
 // Test cases: 
 const head = new node(6);
   head.left = new node(3);
   head.right = new node(9);
 
-  console.log(traverse(head)); // 6, 3, 9
-
+console.log(traverse(head)); // 6, 3, 9
 */
 
 // 6.
-
 /* 
+class node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  } 
+}
+
 function traverse(root) {
   const value = [];
   const queue = [];
@@ -136,36 +250,16 @@ const head = new node(6);
   console.log(traverse(head)); // 6, 3, 9
 */
 
-/* 
-function traverse(root) {
-  const value = [];
-  const queue = [];
-  queue.push(root);
-
-  while (queue.length) {
-    queue.push(current.value);
-  }
-  if (current.left !== null) {
-    queue.push(current.left);
-  }
-  if (current.left !== null) {
-    queue.push(current.right);
-  }
-}
-return values;
-}
-
-// Test cases: 
-const head = new node(6);
-  head.left = new node(3);
-  head.right = new node(9);
-
-  console.log(traverse(head)); // 6, 3, 9
-*/
-
 // 7.
-
 /*
+class node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
 function traverse(root) {
   const value - [];
   const queue = [];
@@ -183,7 +277,7 @@ function traverse(root) {
     }
   }
   return values;
-  }
+}
 
   // Test cases: 
   const head = new node(6);
@@ -195,6 +289,13 @@ function traverse(root) {
 
 // 8.
 /*
+class node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
   functioni traverse(root) {
     const value = [];
     const queue = [];
@@ -212,7 +313,7 @@ function traverse(root) {
     }
   }
   return values; 
-} 
+}
 
 // Test cases: 
 const head = new Node(6); 
@@ -224,6 +325,14 @@ const head = new Node(6);
 
 // 9.
 /* 
+class node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
   function traverse(root) {
     const value = [];
     const queue = [];
@@ -240,18 +349,26 @@ const head = new Node(6);
     }
   }
   return values; 
-}
+} 
 
 const head = new node(6);
   head.left = new node(3);
   head.right = new node(9);
 
   console.log(traverse(head)); // 6, 3, 9 
-
   */
 
 //10.
-/* function traverse(root) {
+/*
+class node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+function traverse(root) {
   const value = [];
   const queue = [];
   queue.push(root);
@@ -276,5 +393,36 @@ head.left = new node(3);
 head.right = new node(9);
 
 console.log(traverse(head)): // 6, 3, 9
-
 */
+
+class node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+function traverse(root) {
+  const value = [];
+  const queue = [];
+  queue.push(root);
+
+  while (current.length) {
+    queue.push(current.value);
+  }
+  if (current.left !== null) {
+    queue.push(current.left);
+  }
+  if (current.right !== null) {
+    queue.push(current.righth);
+  }
+}
+return values;
+
+// Test cases:
+const head = new node(6);
+head.left = new node(3);
+head.right = new node(9);
+
+console.log(traverse(head)); // 6, 3, 9
