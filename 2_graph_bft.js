641cdec149250241d52b63d66897275f68420fe9
Cesar Aguilar <Blueavian9@gmail.com>
2023-07-24 18:55:37 -0700

    Initial commit

--- 2_graph_bft.js
+++ 2_graph_bft.js
@@ -1,53 +1,38 @@
 function traverse(graph, root) {
   const value = [];
   const queue = [];
-  queue.push(root)
+  queue.push(root);
 
-  const visited = new Set
-  visited.add(root)
+  const visited = new Set();
+  visited.add(root);
 
   while (queue.length) {
     // Dequeue a vertex from queue
-    const current = queue.shift() 
-    values.push(current) 
+    const current = queue.shift();
+    values.push(current);
 
     // If not yet visited, mark it as visited, and enqueue it.
-    for (let neighbor of graph([current]) {
+    for (let neighbor of graph[current]) {
       if (!visited.has(neighbor)) {
-        visited.add(neighbor) 
-        queue.push(neighbor)
+        visited.add(neighbor);
+        queue.push(neighbor);
       }
     }
   }
-  return values
+  return values;
 }
 
-
-// Test cases: 
-// We'll be using this simple undirected graph to test run BFT: 
+// Test cases:
+// We'll be using this simple undirected graph to test run BFT:
 // 5 -- 3
 // | \
 // |  2
 // | / \
 // |1   4
-const graph = { 1: [2, 5], 2: [1, 4, 5], 3: [5], 4: [2], 5: [1, 2, 4] }
-console.log(traverse(graph, )); // 1, 2, 5, 4, 3
-console.log(traverse(graph, )); // 4, 2, 1, 5, 3
-console.log(traverse(graph, )); // 5, 1, 2, 3, 4
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+const graph = { 1: [2, 5], 2: [1, 4, 5], 3: [5], 4: [2], 5: [1, 2, 4] };
+console.log(traverse(graph)); // 1, 2, 5, 4, 3
+console.log(traverse(graph)); // 4, 2, 1, 5, 3
+console.log(traverse(graph)); // 5, 1, 2, 3, 4
 
 // function traverse(graph, root) {
 //   const value = [];
